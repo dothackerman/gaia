fcb8434f916ae154e1ff8e78420b8aff8b9699cd
OG <gutori01@students.zhaw.ch>
2026-03-08 10:39:21 +0100

    feat(proposals): move governance parameters to on-chain storage

--- pallets/proposals/src/mock.rs
+++ pallets/proposals/src/mock.rs
@@ -8,31 +8,31 @@ type Block = frame_system::mocking::MockBlock<Test>;
 
 #[frame_support::runtime]
 mod runtime {
-	#[runtime::runtime]
-	#[runtime::derive(
-		RuntimeCall,
-		RuntimeEvent,
-		RuntimeError,
-		RuntimeOrigin,
-		RuntimeFreezeReason,
-		RuntimeHoldReason,
-		RuntimeSlashReason,
-		RuntimeLockId,
-		RuntimeTask,
-		RuntimeViewFunction
-	)]
-	pub struct Test;
+    #[runtime::runtime]
+    #[runtime::derive(
+        RuntimeCall,
+        RuntimeEvent,
+        RuntimeError,
+        RuntimeOrigin,
+        RuntimeFreezeReason,
+        RuntimeHoldReason,
+        RuntimeSlashReason,
+        RuntimeLockId,
+        RuntimeTask,
+        RuntimeViewFunction
+    )]
+    pub struct Test;
 
-	#[runtime::pallet_index(0)]
-	pub type System = frame_system::Pallet<Test>;
+    #[runtime::pallet_index(0)]
+    pub type System = frame_system::Pallet<Test>;
 
-	#[runtime::pallet_index(1)]
-	pub type Proposals = gaia_proposals::Pallet<Test>;
+    #[runtime::pallet_index(1)]
+    pub type Proposals = gaia_proposals::Pallet<Test>;
 }
 
 #[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
 impl frame_system::Config for Test {
-	type Block = Block;
+    type Block = Block;
 }
 
 // ---------------------------------------------------------------------------
@@ -40,24 +40,24 @@ impl frame_system::Config for Test {
 // ---------------------------------------------------------------------------
 
 thread_local! {
-	static ACTIVE_MEMBERS: RefCell<BTreeSet<u64>> = RefCell::new(BTreeSet::new());
+    static ACTIVE_MEMBERS: RefCell<BTreeSet<u64>> = RefCell::new(BTreeSet::new());
 }
 
 pub struct MockMembership;
 
 impl MockMembership {
-	pub fn add(who: u64) {
-		ACTIVE_MEMBERS.with(|m| m.borrow_mut().insert(who));
-	}
-	pub fn remove(who: u64) {
-		ACTIVE_MEMBERS.with(|m| m.borrow_mut().remove(&who));
-	}
+    pub fn add(who: u64) {
+        ACTIVE_MEMBERS.with(|m| m.borrow_mut().insert(who));
+    }
+    pub fn remove(who: u64) {
+        ACTIVE_MEMBERS.with(|m| m.borrow_mut().remove(&who));
+    }
 }
 
 impl gaia_proposals::MembershipChecker<u64> for MockMembership {
-	fn is_active_member(account: &u64) -> bool {
-		ACTIVE_MEMBERS.with(|m| m.borrow().contains(account))
-	}
+    fn is_active_member(account: &u64) -> bool {
+        ACTIVE_MEMBERS.with(|m| m.borrow().contains(account))
+    }
 }
 
 // ---------------------------------------------------------------------------
@@ -65,24 +65,24 @@ impl gaia_proposals::MembershipChecker<u64> for MockMembership {
 // ---------------------------------------------------------------------------
 
 thread_local! {
-	static TREASURY_FAILS: RefCell<bool> = const { RefCell::new(false) };
+    static TREASURY_FAILS: RefCell<bool> = const { RefCell::new(false) };
 }
 
 pub struct MockTreasury;
 
 impl MockTreasury {
-	pub fn set_fail(fail: bool) {
-		TREASURY_FAILS.with(|f| *f.borrow_mut() = fail);
-	}
+    pub fn set_fail(fail: bool) {
+        TREASURY_FAILS.with(|f| *f.borrow_mut() = fail);
+    }
 }
 
 impl gaia_proposals::TreasuryHandler<u64, u64> for MockTreasury {
-	fn disburse(_to: &u64, _amount: u64) -> DispatchResult {
-		if TREASURY_FAILS.with(|f| *f.borrow()) {
-			return Err(sp_runtime::DispatchError::Other("mock treasury failure"));
-		}
-		Ok(())
-	}
+    fn disburse(_to: &u64, _amount: u64) -> DispatchResult {
+        if TREASURY_FAILS.with(|f| *f.borrow()) {
+            return Err(sp_runtime::DispatchError::Other("mock treasury failure"));
+        }
+        Ok(())
+    }
 }
 
 // ---------------------------------------------------------------------------
@@ -90,11 +90,10 @@ impl gaia_proposals::TreasuryHandler<u64, u64> for MockTreasury {
 // ---------------------------------------------------------------------------
 
 impl gaia_proposals::Config for Test {
-	type RuntimeEvent = RuntimeEvent;
-	type Balance = u64;
-	type Membership = MockMembership;
-	type Treasury = MockTreasury;
-	type VotingPeriod = frame_support::traits::ConstU64<10>;
+    type RuntimeEvent = RuntimeEvent;
+    type Balance = u64;
+    type Membership = MockMembership;
+    type Treasury = MockTreasury;
 }
 
 // ---------------------------------------------------------------------------
@@ -113,19 +112,32 @@ pub const DAVE: u64 = 4;
 // ---------------------------------------------------------------------------
 
 pub fn new_test_ext() -> sp_io::TestExternalities {
-	let storage = frame_system::GenesisConfig::<Test>::default()
-		.build_storage()
-		.unwrap();
+    let mut storage = frame_system::GenesisConfig::<Test>::default()
+        .build_storage()
+        .unwrap();
 
-	let mut ext = sp_io::TestExternalities::new(storage);
-	ext.execute_with(|| {
-		System::set_block_number(1);
-		// Seed genesis members.
-		MockMembership::add(ALICE);
-		MockMembership::add(BOB);
-		MockMembership::add(CHARLIE);
-		// Reset treasury flag.
-		MockTreasury::set_fail(false);
-	});
-	ext
+    gaia_proposals::GenesisConfig::<Test> {
+        proposal_voting_period: 10,
+        execution_delay: 0,
+        standard_approval_numerator: 1,
+        standard_approval_denominator: 2,
+        governance_approval_numerator: 4,
+        governance_approval_denominator: 5,
+        constitutional_approval_numerator: 9,
+        constitutional_approval_denominator: 10,
+    }
+    .assimilate_storage(&mut storage)
+    .unwrap();
+
+    let mut ext = sp_io::TestExternalities::new(storage);
+    ext.execute_with(|| {
+        System::set_block_number(1);
+        // Seed genesis members.
+        MockMembership::add(ALICE);
+        MockMembership::add(BOB);
+        MockMembership::add(CHARLIE);
+        // Reset treasury flag.
+        MockTreasury::set_fail(false);
+    });
+    ext
 }
